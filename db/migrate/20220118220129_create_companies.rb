1e2e2d250421d701d4805d92d2246f95da0bf7e2
EC2 Default User <ec2-user@ip-172-30-1-228.ec2.internal>
2022-01-18 22:56:47 +0000

    Companies Scaffold

---
+++ db/migrate/20220118220129_create_companies.rb
@@ -0,0 +1,24 @@
+class CreateCompanies < ActiveRecord::Migration[7.0]
+  def up
+    create_table :companies do |t|
+      t.string :name, null: false
+      t.string :url, null: false
+      t.boolean :active, null: false, default: true
+
+      t.timestamps
+    end
+
+    execute <<-SQL
+      ALTER TABLE companies 
+        MODIFY COLUMN id INT UNSIGNED
+    SQL
+
+    add_index :companies, [:active, :id, :url]
+    add_index :companies, :url, unique: true
+
+  end
+
+  def down
+    drop_table :companies
+  end
+end
