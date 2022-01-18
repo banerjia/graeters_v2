34fe450f2443c396332185df33679d945f65f18f
EC2 Default User <ec2-user@ip-172-30-1-228.ec2.internal>
2022-01-18 23:14:40 +0000

    Adding DEV Host

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module GraetersV2
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.hosts << "abhishekbanerji.com"
   end
 end
