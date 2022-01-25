1393f99cff77d0bfd78c02799f0d8d4cd4278ba2
Abs <webdesign.banerji@gmail.com>
2022-01-25 15:06:52 -0500

    Basic Sitewide Styling
    Created a basic sitewide page template with styling. Important notes:
    
    - Google fonts have been used - Open Sans and Quicksand
    - Footer template accidentally turned out good

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
     def index
+        @Page_Title = "Home Page"
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   scope '/graeters' do
     namespace :manage do
       resources :retailers
