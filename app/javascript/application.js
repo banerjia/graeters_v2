305ddb49450977c583a24d18c3ff7ec3b82dcf42
Abs <webdesign.banerji@gmail.com>
2022-01-26 11:34:06 -0500

    Bootstrap Popover Fix
    This update fixes issues with Bootstrap Popover not working properly.
    There were three causes for this not working properly:
    
    - popperjs/core was not installed with NPM
    - The relevant code was not present in application.js to instantiate the
      necesary objects
    - process.env.NODE was not defined outside of createPopper.js. To fix
      this debug code was added to application.js to declare this object to
    prevent an Uncaught ReferenceError

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,17 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import * as bootstrap from "bootstrap" 
+
+// DEBUG CODE 
+window.process = {
+    env: {
+        NODE_ENV: 'development'
+    }
+}
+// END DEBUG CODE
+
+let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+
