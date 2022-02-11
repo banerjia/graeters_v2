e1c91a86c614a015a189f2dbb52ff66b2bbe01a4
Abs <webdesign.banerji@gmail.com>
2022-02-11 17:03:15 -0500

    Beginning Store Setup

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,6 +1,43 @@
+require 'net/http'
+require 'json'
+require 'uri'
+
 class Store < ApplicationRecord
+
+  # Associations
   belongs_to :retailer, counter_cache: true
   belongs_to :state
 
   has_many :comments, as: :commentable
+
+  # Validations
+  validates :name, :addr_ln_1, :city, :state_id, presence: true
+
+  # Callbacks
+  after_validation :set_lat_lng, on: [ :create, :update]
+  after_create :set_retailer_last_updated
+
+
+  # Private Functions
+  private
+  def set_lat_lng
+      address = "%s, %s, %s" % [self.addr_ln_1, self.city, self.state.state_code]
+      address = CGI.escape(address)
+
+      gmaps_uri = URI('https://maps.googleapis.com/maps/api/geocode/json?address='+ address + '&key=A')
+
+      gmaps_resp_obj = Net::HTTP.get_response(gmaps_uri)
+
+      gmaps_resp = JSON.parse(gmaps_resp_obj.body)
+
+      gmaps_location = gmaps_resp['results'].first['geometry']['location']
+
+      self.latitude, self.longitude = gmaps_location['lat'], gmaps_location['lng']
+  end
+
+  def set_retailer_last_updated
+    self.retailer.latest_store_add_date = DateTime.now
+    self.retailer.save
+  end
+
 end
